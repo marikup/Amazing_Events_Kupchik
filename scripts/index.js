227bbc018b198c3aa3f9cf206ef1c1c02f1a82a0
Maria <mkupchik@hotmail.com>
2023-03-10 14:38:27 -0300

    arreglos

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,20 @@
 const containerCard = document.getElementById('card1')
 let card = '';
 
-for (evento of data.events){
+for (evento of data.events) {
     card += ` <div class="card text-white bg-dark " style="width: 18rem;margin:.5rem;" >
-        <img src="${evento.image}" class="card-img-top" alt="...">
+   <div class ="card-img">
+      <img src="${evento.image}" class="card-img-top" alt="...">
+        </div>
         <div class="card-body">
             <h5 class="card-title">${evento.name}</h5>
             <p class="card-text">${evento.description}</p>
             <p class="card-text">${evento.date}</p>
-            <p class="card-text-price"><i class="bi bi-tag-fill"></i> Price:${evento.price}</p>
+            </div>
             <div class="contenedor-btn">
-            <a href="#" class="btn-card btn btn-danger border border-light">Detail </a>
-          </div>
-    </div>
+            <p class="card-text-price"><i class="bi bi-tag-fill"></i> Price:${evento.price}</p>
+         <a href="#" class="btn-card btn btn-danger border border-light">Detail </a>
+         </div>
 </div> `
 }
 containerCard.innerHTML = card
